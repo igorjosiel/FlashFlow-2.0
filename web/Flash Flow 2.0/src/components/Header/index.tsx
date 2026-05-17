def488dd084024865d2ce48a95ea0b738dbe3a00
igorjosiel <igorjosielvitalino@gmail.com>
2026-05-17 11:55:00 -0300

    feat: criando o projeto front-end da aplicação.

---
+++ web/Flash Flow 2.0/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import flashcardIcon from "../../assets/flashcards-icon.svg";
+
+function Header() {
+    return (
+        <header className="flex justify-between py-5 px-8">
+        <div className="flex justify-between w-41.75 items-center">
+          <img src={flashcardIcon} alt="Logo da aplicação Flash Flow" />
+          <span className="font-manrope font-extrabold text-[24px] leading-8 tracking-[-0.6px] text-surface-dark">Flash Flow</span>
+        </div>
+        
+        <button className="p-6 bg-linear-to-br from-linear-start to-linear-end font-manrope font-bold text-[14px] leading-5 text-white h-10 w-38 
+          flex justify-center items-center rounded-3xl cursor-pointer">
+          Novo Flashcard
+        </button>
+      </header>
+    )
+}
+
+export default Header;

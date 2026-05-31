709360e36b89133ccb4ecdf0dc41c40d272bbea9
igorjosiel <igorjosielvitalino@gmail.com>
2026-05-31 14:39:22 -0300

    feat: adiciona os serviços para buscar os dados da API de categories e flashcards.

--- web/src/services/flashcards.ts
+++ web/src/services/flashcards.ts
@@ -1,7 +1,13 @@
 import { api } from './api';
 
-export async function getFlashcards() {
-  const response = await api.get('/flashcards')
+export async function getFlashcards(selectedCategory: string) {
+  if (selectedCategory) {
+    const response = await api.get(`/flashcards?category=${selectedCategory}`);
+
+    return response.data;
+  }
+  
+  const response = await api.get(`/flashcards`);
 
   return response.data;
 }

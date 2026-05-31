3ac44700eb59fefc10859e3fc25ae9139c7ccb88
igorjosiel <igorjosielvitalino@gmail.com>
2026-05-31 10:41:38 -0300

    fix: realiza ajustes nos roteadores de Flashcards e categories.

--- server/src/routes/Categories.ts
+++ server/src/routes/Categories.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import CategoryController from "../controller/categories";
+import CategoryController from "../controller/Categories";
 
 const categoriesRouter = Router();
 
---
+++ server/src/routes/Flashcards.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import FlashcardController from "../controller/Flashcards";
+import validateRequiredFieldsFlashcards from "../middlewares/ValidateRequiredFieldsFlashcards";
+
+const flashcardsRouter = Router();
+
+const flashcardController = new FlashcardController();
+
+flashcardsRouter.get("/flashcards", (req, res) => flashcardController.getAll(req, res));
+flashcardsRouter.post("/flashcards", validateRequiredFieldsFlashcards, (req, res) => flashcardController.create(req, res));
+flashcardsRouter.put("/flashcards/:id", validateRequiredFieldsFlashcards, (req, res) => flashcardController.update(req, res));
+flashcardsRouter.delete("/flashcards/:id", (req, res) => flashcardController.delete(req, res));
+
+export default flashcardsRouter;
